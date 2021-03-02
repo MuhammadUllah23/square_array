6d3486ac9e319e0f2fd63845ef2f5050cf71a960
Muhammad <muhammadhullah23@gmail.com>
2021-03-01 19:55:25 -0500

    commit tree 8da1b42585f67f4cb25ac8656483e0e62debe157

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  square_numbers = []
+  numbers.each do |num|
+    square_numbers << (num ** 2)
+  end
+  return square_numbers
 end
+
+
